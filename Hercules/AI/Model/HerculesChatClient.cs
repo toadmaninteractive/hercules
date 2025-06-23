369f8a09313e48d669f40c886a73acdc0fcdf555
andrey.tsirulev <thehangedman@mail.ru>
2025-06-23 17:05:08 +0300

    Arrange file locations in AI module

--- Hercules/AI/Model/HerculesChatClient.cs
+++ Hercules/AI/Model/HerculesChatClient.cs
@@ -4,7 +4,6 @@ using Anthropic.SDK.Messaging;
 using ICSharpCode.AvalonEdit.Document;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hercules.AI
--- Hercules/AI/View/AiSettingsTabView.xaml.cs
+++ Hercules/AI/View/AiSettingsTabView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hercules.AI.Model
+namespace Hercules.AI.View
 {
     /// <summary>
     /// Interaction logic for AiSettingsTabView.xaml
